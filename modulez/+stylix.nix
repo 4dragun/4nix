275f3002f131e2679c9a5035415b68d03d0c1a29
4 <>
2025-03-18 11:51:43 +0530

    new walll

--- modulez/+stylix.nix
+++ modulez/+stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    image = ../wallz/Lowpoly_Street.png;
+    image = ../wallz/Backyard.png;
     polarity = "dark";
     fonts = {
       sizes = {
